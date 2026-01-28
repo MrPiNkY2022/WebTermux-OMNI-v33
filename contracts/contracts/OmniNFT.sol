7eabd04b9dd2a75f4ef75016af431e60bfc0e9e6
MrPiNkY2022 <97333310+MrPiNkY2022@users.noreply.github.com>
2026-01-28 03:06:50 -0500

    Integrate Chainlink VRF and Keeper in OmniNFT
    Refactor OmniNFT contract to integrate Chainlink VRF and Keeper functionalities while removing OpenZeppelin dependencies.

--- contracts/contracts/OmniNFT.sol
+++ contracts/contracts/OmniNFT.sol
@@ -1,40 +1,23 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+// Add imports
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
 
-import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+contract OmniNFT is ... , VRFConsumerBaseV2, KeeperCompatibleInterface {
+    // VRF variables
+    uint64 s_subscriptionId;
+    address vrfCoordinator = 0x...; // Sepolia: 0x8103B0A8A...
+    bytes32 keyHash = 0x...;
+    uint32 callbackGasLimit = 100000;
 
-contract OmniNFT is ERC721Upgradeable, AccessControlUpgradeable, UUPSUpgradeable {
-    string private _baseURIextended;
-    bool public soulbound; // Non-transferable mode
-
-    function initialize() public initializer {
-        __ERC721_init("WEBTERMUX OMNI V33", "WTONFT");
-        __AccessControl_init();
-        __UUPSUpgradeable_init();
-
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _baseURIextended = "ipfs://YOUR_CID/";
+    function requestRandomMint() external {
+        requestId = COORDINATOR.requestRandomWords(...);
     }
 
-    function mint(address to, uint256 tokenId) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        _safeMint(to, tokenId);
+    function fulfillRandomWords(...) internal override {
+        // Mint with randomness
     }
 
-    function setSoulbound(bool _soulbound) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        soulbound = _soulbound;
-    }
-
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
-        internal override virtual {
-        require(!soulbound || from == address(0), "Soulbound");
-        super._beforeTokenTransfer(from, to, tokenId, batchSize);
-    }
-
-    function _baseURI() internal view override returns (string memory) {
-        return _baseURIextended;
-    }
-
-    function _authorizeUpgrade(address) internal override onlyRole(DEFAULT_ADMIN_ROLE) {}
+    // Keeper for auto-rewards
+    function checkUpkeep(...) external override returns (bool, bytes memory) {}
+    function performUpkeep(...) external override {}
 }
